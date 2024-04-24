2adfe84b4dfb544aa56628559416b7597563cbc4
Peace Duck <mypeaceduck@proton.me>
2024-04-25 01:14:26 +0300

    Add check that the owner is not zero

--- src/Toqen.sol
+++ src/Toqen.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.20;
 
 /*
- *   ooooooooooooo                                            
- *   8'   888   `8                                            
- *        888       .ooooo.   .ooooo oo  .ooooo.  ooo. .oo.   
- *        888      d88' `88b d88' `888  d88' `88b `888P"Y88b  
- *        888      888   888 888   888  888ooo888  888   888  
- *        888      888   888 888   888  888    .o  888   888  
- *       o888o     `Y8bod8P' `V8bod888  `Y8bod8P' o888o o888o 
- *                                 888.                       
- *                                 8P'                        
- *                                 "                          
-*/
+ *   ooooooooooooo
+ *   8'   888   `8
+ *        888       .ooooo.   .ooooo oo  .ooooo.  ooo. .oo.
+ *        888      d88' `88b d88' `888  d88' `88b `888P"Y88b
+ *        888      888   888 888   888  888ooo888  888   888
+ *        888      888   888 888   888  888    .o  888   888
+ *       o888o     `Y8bod8P' `V8bod888  `Y8bod8P' o888o o888o
+ *                                 888.
+ *                                 8P'
+ *                                 "
+ */
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
@@ -28,7 +28,7 @@ contract ERC20Toqen is ERC20, ERC20Permit, ReentrancyGuard {
     /// @notice The price per token in Wei
     uint256 public immutable tokenPrice;
 
-    /** 
+    /**
      * @notice Creates a new ERC20 token
      * @param _owner The owner of the token
      * @param _name The name of the token
@@ -43,23 +43,27 @@ contract ERC20Toqen is ERC20, ERC20Permit, ReentrancyGuard {
         uint256 _maxSupply,
         uint256 _tokenPrice
     ) ERC20(_name, _symbol) ERC20Permit(_name) {
+        require(_owner != address(0));
         owner = _owner;
         maxSupply = _maxSupply;
         tokenPrice = _tokenPrice;
     }
 
-    /** 
+    /**
      * @notice Mints tokens to a specified account
      * @param account The account to mint tokens to
      * @param amount The amount of tokens to mint
-     * @dev This function requires the contract to have a non-zero token price 
-     * and for the message value to cover the price of the desired amount of 
+     * @dev This function requires the contract to have a non-zero token price
+     * and for the message value to cover the price of the desired amount of
      * tokens
      */
     function mint(address account, uint256 amount) public payable nonReentrant {
         require(totalSupply() + amount <= maxSupply, "Exceeds max supply");
 
-        if (tokenPrice > 0 && msg.value != amount * tokenPrice / 10**decimals()) {
+        if (
+            tokenPrice > 0 &&
+            msg.value != (amount * tokenPrice) / 10 ** decimals()
+        ) {
             revert("Insufficient Ether sent");
         }
 
@@ -69,12 +73,14 @@ contract ERC20Toqen is ERC20, ERC20Permit, ReentrancyGuard {
     /// @notice Receives Ether and mints tokens according to the token price
     receive() external payable {
         require(tokenPrice > 0, "Cannot send ETH with zero token price");
-        mint(msg.sender, msg.value * 10**decimals() / tokenPrice);
+        mint(msg.sender, (msg.value * 10 ** decimals()) / tokenPrice);
     }
 
     /// @notice Withdraws all Ether in the contract to a owner address
     function withdraw() public nonReentrant {
-        (bool sent, ) = payable(address(owner)).call{value: address(this).balance}("");
+        (bool sent, ) = payable(address(owner)).call{
+            value: address(this).balance
+        }("");
         require(sent, "Failed to send Ether");
     }
 }
@@ -93,7 +99,7 @@ contract ERC721Toqen is ERC721, ReentrancyGuard {
     /// @notice The current token ID to be minted next
     uint256 public totalSupply;
 
-    /** 
+    /**
      * @notice Creates a new ERC721 token
      * @param _owner The owner of the token
      * @param _name The name of the token
@@ -110,6 +116,7 @@ contract ERC721Toqen is ERC721, ReentrancyGuard {
         uint256 _tokenPrice,
         string memory baseURI_
     ) ERC721(_name, _symbol) {
+        require(_owner != address(0));
         owner = _owner;
         maxSupply = _maxSupply;
         tokenPrice = _tokenPrice;
@@ -122,12 +129,12 @@ contract ERC721Toqen is ERC721, ReentrancyGuard {
         return baseURI;
     }
 
-    /** 
+    /**
      * @notice Mints tokens to a specified account
      * @param account The account to mint tokens to
      * @param amount The amount of tokens to mint
-     * @dev This function requires the contract to have a non-zero token price 
-     * and for the message value to cover the price of the desired amount of 
+     * @dev This function requires the contract to have a non-zero token price
+     * and for the message value to cover the price of the desired amount of
      * tokens
      */
     function mint(address account, uint256 amount) public payable nonReentrant {
@@ -150,7 +157,9 @@ contract ERC721Toqen is ERC721, ReentrancyGuard {
 
     /// @notice Withdraws all Ether in the contract to a owner address
     function withdraw() public nonReentrant {
-        (bool sent, ) = payable(address(owner)).call{value: address(this).balance}("");
+        (bool sent, ) = payable(address(owner)).call{
+            value: address(this).balance
+        }("");
         require(sent, "Failed to send Ether");
     }
 }
@@ -163,7 +172,7 @@ contract Toqen {
     /// @notice Creates the contract
     constructor() {}
 
-    /** 
+    /**
      * @notice Creates a new ERC20 token and registers it
      * @param name The name of the ERC20 token
      * @param symbol The symbol of the ERC20 token
@@ -183,7 +192,7 @@ contract Toqen {
         emit TokenCreated(address(token), msg.sender);
     }
 
-    /** 
+    /**
      * @notice Creates a new ERC721 token and registers it
      * @param name The name of the ERC721 token
      * @param symbol The symbol of the ERC721 token
@@ -201,7 +210,14 @@ contract Toqen {
         uint256 tokenPrice,
         string memory baseURI
     ) public returns (ERC721Toqen token) {
-        token = new ERC721Toqen(msg.sender, name, symbol, maxSupply, tokenPrice, baseURI);
+        token = new ERC721Toqen(
+            msg.sender,
+            name,
+            symbol,
+            maxSupply,
+            tokenPrice,
+            baseURI
+        );
         emit TokenCreated(address(token), msg.sender);
     }
 }
